e4e33a766995fce4cdedfb584f8ed25eb2489ee4
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2024-01-25 12:15:11 -0800

    made small change to output things on the same line

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -38,9 +38,9 @@ namespace COMP003A.Assignment2
             Console.WriteLine("*********************************************************");
             Console.WriteLine("Math Section");
             Console.WriteLine("**********************************************************");
-            Console.WriteLine("Enter an integer for integer 1");
+            Console.Write("Enter an integer for integer 1: ");
             int integer1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter an integer for integer 2:");
+            Console.Write("Enter an integer for integer 2: ");
             int integer2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"{integer1} + {integer2} = {integer1 + integer2}");
             Console.WriteLine($"{integer1} - {integer2} = {integer1 - integer2}");
@@ -55,7 +55,7 @@ namespace COMP003A.Assignment2
             Console.WriteLine("*********************************************************");
             Console.WriteLine("Circle Area & Circumference Calculator Section");
             Console.WriteLine("*********************************************************");
-            Console.WriteLine("Enter Radius:");
+            Console.Write("Enter Radius: ");
             double radius = Convert.ToDouble(Console.ReadLine());
             double area = Math.PI * Math.Pow(radius, 2);
             double circumference = 2 * Math.PI * radius;
